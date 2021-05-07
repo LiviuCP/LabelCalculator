0bc2f2f1cf6c289b723d1c555bebf5b18f2ba679
Liviu C. Popa <liviucst@gmail.com>
2021-05-07 21:36:38 +0300

    Rearranged some constants in labelutils.h

--- labelutils.h
+++ labelutils.h
@@ -8,14 +8,21 @@
 
 /* --CONSTANTS-- */
 
+// offset used for calculating the row number based on payload index (header row is ignored and row numbering starts at 1)
+static constexpr int c_RowNumberOffset{2};
+
+// offset used for calculating the column number of the first device port parameters (preceding columns are device type and device U position)
+static constexpr int c_DevicePortParamsColumnOffset{2};
+
 static constexpr int c_MaxNrOfRackUnits{50};
-static constexpr int c_RowNumberOffset{2}; // offset used for calculating the row number based on payload index (header row is ignored and row numbering starts at 1)
-static constexpr int c_DevicePortParamsColumnOffset{2}; // offset used for calculating the column number of the first device port parameters (preceding columns are device type and device U position)
 static constexpr int c_DevicesPerConnectionInputRowCount{2};
 
 static const std::string c_CablePartNumberPlaceholder{"CBL_PART_NR"};
-static const std::string c_InputHeader{"__Cable part number__,__Source device type__,__Source U number__,__Parameter1__,__Parameter2__,__Parameter3__,__Destination device type__,__Destination U number__,__Parameter1__,__Parameter2__,__Parameter3__"};
-static const std::string c_OutputHeader{"__Item number__,__Cable part number__,__Source device description__,__Source label__,__Destination device description__,__Destination label__"};
+static const std::string c_InputHeader{"__Cable part number__,__Source device type__,__Source U number__,__Parameter1__,__Parameter2__,"
+                                       "__Parameter3__,__Destination device type__,__Destination U number__,__Parameter1__,__Parameter2__,__Parameter3__"};
+static const std::string c_OutputHeader{"__Item number__,__Cable part number__,__Source device description__,"
+                                        "__Source label__,__Destination device description__,__Destination label__"};
+
 static const std::string c_UnknownPortTypeErrorText{"ERROR: UNKNOWN PORT TYPE (PLEASE CHECK INPUT FILE connectioninput.csv)"};
 static const std::string c_UnknownModuleTypeErrorText{"ERROR: UNKNOWN MODULE TYPE (PLEASE CHECK INPUT FILE connectioninput.csv)"};
 static const std::string c_InvalidPlacementErrorText{"ERROR: THE PLACEMENT TYPE YOU HAVE CHOSEN DOES NOT EXIST. PLEASE REVIEW INPUT FILE (connectioninput.csv)."};
